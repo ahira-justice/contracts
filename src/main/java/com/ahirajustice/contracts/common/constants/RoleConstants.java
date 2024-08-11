e33373839978018661aca81d911feca5164ff74d
Ahira Adefokun <ahira@adefokun.com>
2024-08-11 22:24:19 +0100

    Add role constants
    See 4510a95.

---
+++ src/main/java/com/ahirajustice/contracts/common/constants/RoleConstants.java
@@ -0,0 +1,13 @@
+package com.ahirajustice.contracts.common.constants;
+
+public class RoleConstants {
+
+    public static final String AUTH_PREFIX = "hasRole('";
+    public static final String AUTH_SUFFIX = "')";
+
+    // User authorities
+    public static final String SUPER_ADMIN = "SUPER_ADMIN";
+    public static final String CONTRACTOR = "CONTRACTOR";
+    public static final String CLIENT = "CLIENT";
+
+}
